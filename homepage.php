9e1f19c2a0baf3804f426d89665ef81815131c8e
Mufaqar Islam <mufaqar@gmail.com>
2022-12-08 15:21:04 +0500

    gallery

--- homepage.php
+++ homepage.php
@@ -358,53 +358,12 @@ get_header('home');
 
 
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-	<script>
-		!window.jQuery && document.write('<script src="jquery-1.4.3.min.js"><\/script>');
-	</script>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/resources/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/resources/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/resources/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
     <script type="text/javascript">
 		$(document).ready(function() {
-			/*
-			*   Examples - images
-			*/
-
-			$("a#example1").fancybox();
-
-			$("a#example2").fancybox({
-				'overlayShow'	: false,
-				'transitionIn'	: 'elastic',
-				'transitionOut'	: 'elastic'
-			});
-
-			$("a#example3").fancybox({
-				'transitionIn'	: 'none',
-				'transitionOut'	: 'none'	
-			});
-
-			$("a#example4").fancybox({
-				'opacity'		: true,
-				'overlayShow'	: false,
-				'transitionIn'	: 'elastic',
-				'transitionOut'	: 'none'
-			});
-
-			$("a#example5").fancybox();
-
-			$("a#example6").fancybox({
-				'titlePosition'		: 'outside',
-				'overlayColor'		: '#000',
-				'overlayOpacity'	: 0.9
-			});
-
-			$("a#example7").fancybox({
-				'titlePosition'	: 'inside'
-			});
-
-			$("a#example8").fancybox({
-				'titlePosition'	: 'over'
-			});
+			
 
 			$("a[rel=example_group]").fancybox({
 				'transitionIn'		: 'none',
@@ -415,32 +374,6 @@ get_header('home');
 				}
 			});
 
-			/*
-			*   Examples - various
-			*/
-
-			$("#various1").fancybox({
-				'titlePosition'		: 'inside',
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none'
-			});
-
-			$("#various2").fancybox();
-
-			$("#various3").fancybox({
-				'width'				: '75%',
-				'height'			: '75%',
-				'autoScale'			: false,
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none',
-				'type'				: 'iframe'
-			});
-
-			$("#various4").fancybox({
-				'padding'			: 0,
-				'autoScale'			: false,
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none'
-			});
+			
 		});
 	</script>
