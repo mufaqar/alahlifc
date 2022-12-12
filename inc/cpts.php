8766f9607f285005c47a842efa07127b18cf8d74
Mufaqar Islam <mufaqar@gmail.com>
2022-12-12 19:12:14 +0500

    functinos

--- inc/cpts.php
+++ inc/cpts.php
@@ -276,3 +276,87 @@ register_post_type( "womain_news", $args );
 add_action( 'init', 'cptui_register_my_cpts_womain_news' );
 
 
+function cptui_register_my_cpts_matchresult() {
+
+	/**
+	 * Post Type: Match results.
+	 */
+
+	$labels = [
+		"name" => __( "Match results", "Hello-Elementor-child" ),
+		"singular_name" => __( "Match result", "Hello-Elementor-child" ),
+	];
+
+	$args = [
+		"label" => __( "Match results", "Hello-Elementor-child" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "matchresult", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "matchresult", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_matchresult' );
+
+
+
+function cptui_register_my_cpts_management() {
+
+	/**
+	 * Post Type: Managements.
+	 */
+
+	$labels = [
+		"name" => __( "Managements", "Hello-Elementor-child" ),
+		"singular_name" => __( "Management", "Hello-Elementor-child" ),
+	];
+
+	$args = [
+		"label" => __( "Managements", "Hello-Elementor-child" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "management", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "management", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_management' );
+
+
+
