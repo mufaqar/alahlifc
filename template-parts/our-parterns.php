e5622c0a70ed6b5227bf5d02144d2b35f59d7ddf
Mufaqar Islam <mufaqar@gmail.com>
2022-12-12 19:42:08 +0500

    رياضة المرأة

--- template-parts/our-parterns.php
+++ template-parts/our-parterns.php
@@ -15,7 +15,6 @@
                     )); 
                     if (have_posts()) :  while (have_posts()) : the_post(); ?>
                     <div class="col partner ">
-
                         <?php if ( has_post_thumbnail() ) {
                                                 the_post_thumbnail('full');
                                             } else { ?>
