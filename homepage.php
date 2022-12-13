8d44c4f9ed432495a702e722981fdabfa010872a
Mufaqar Islam <mufaqar@gmail.com>
2022-12-13 14:48:32 +0500

    rtl

--- homepage.php
+++ homepage.php
@@ -7,19 +7,13 @@
 
     ?>
 
-
- 
-
     <?php get_template_part('template-parts/our' , 'blog') ?>
     <?php get_template_part('template-parts/our' , 'matches') ?>
     <?php get_template_part('template-parts/our' , 'traning') ?>    
     <?php get_template_part('template-parts/first' , 'team') ?>
     <?php get_template_part('template-parts/woman' , 'sports') ?>
     <?php get_template_part('template-parts/our' , 'management') ?>    
-    <?php get_template_part('template-parts/our' , 'parterns') ?>
-
-
-     
+    <?php get_template_part('template-parts/our' , 'parterns') ?>     
 
     <?php get_footer()?>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
